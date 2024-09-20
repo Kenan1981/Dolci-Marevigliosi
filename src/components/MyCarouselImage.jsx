7abc063106c723d06ff8f4368d1ccb0421b97579
Sefa Eyer <eyersefa@gmail.com>
2024-09-20 23:49:56 +0300

    20.09.v1

---
+++ src/components/MyCarouselImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyCarouselImage = () => {
+  return (
+    <img
+      className="d-block w-100"
+      src="/image-tiramisu-tablet.jpg"
+      alt="Carousel slide"
+    />
+  )
+}
